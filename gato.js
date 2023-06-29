55f74c5af177b2e7e21f4a8c014487430455a7ca
Danna <gutierrezdanna119@gmail.com>
2023-06-29 17:32:20 -0600

    Avances 29-06-2023

--- gato.js
+++ gato.js
@@ -8,9 +8,10 @@ icon.onclick = function () {
   }
 };
 
-let cam = document.getElementsByClassName("box");
+let cam = document.querySelectorAll(".box");
 let tab = document.getElementById("tablero");
 let Btn = document.getElementById("restartBtn");
+let playerM = "O";
 
 const vari = [
   ["1", "2", "3"],
@@ -21,7 +22,10 @@ const vari = [
 function turnoO() {
   let camposVacios = Array.from(cam).filter(filtroVacios);
   let numero = Math.floor(Math.random() * camposVacios.length);
-  camposVacios[numero].innerHTML = "O";
+  camposVacios[numero].innerHTML = playerM;
+  if (Winner(playerM)) {
+    console.log(`${playerM} ha ganado`);
+  }
 }
 
 function filtroVacios(campo) {
@@ -32,141 +36,37 @@ for (let i = 0; i < 9; i++) {
   cam[i].addEventListener("click", function () {
     if (cam[i].textContent === "") {
       cam[i].innerHTML = "X";
-      setTimeout(() => {
-        turnoO();
-      }, 1000);
+      turnoO();
     }
-
-    console.log(cam);
-
-    if (
-      cam[0].textContent == "X" &&
-      cam[1].textContent == "X" &&
-      cam[2].textContent == "X"
-    ) {
-      alert("GANASTE ARRIBA EN LA FILA");
-    }
-
-    if (
-      cam[3].textContent == "X" &&
-      cam[4].textContent == "X" &&
-      cam[5].textContent == "X"
-    ) {
-      alert("GANASTE EN LA FILA DEL MEDIO");
-
-      for (let i = 0; i < 1; i++) {
-        console.log("holaaaaa");
-        break;
-      }
-    }
-
-    if (
-      cam[6].textContent == "X" &&
-      cam[7].textContent == "X" &&
-      cam[8].textContent == "X"
-    ) {
-      alert("GANASTE EN LA FILA DE ABAJO");
-    }
-
-    if (
-      cam[0].textContent == "X" &&
-      cam[3].textContent == "X" &&
-      cam[6].textContent == "X"
-    ) {
-      alert("GANASTE EN LA PRIMERA FILA HORIZONTAL");
-    }
-
-    if (
-      cam[1].textContent == "X" &&
-      cam[4].textContent == "X" &&
-      cam[7].textContent == "X"
-    ) {
-      alert("GANASTE EN LA FILA VERTICAL DEL MEDIO");
-    }
-
-    if (
-      cam[2].textContent == "X" &&
-      cam[5].textContent == "X" &&
-      cam[8].textContent == "X"
-    ) {
-      alert("GANASTE EN LA FILA HORIZONATL DERECHA");
-    }
-    if (
-      cam[0].textContent == "X" &&
-      cam[4].textContent == "X" &&
-      cam[8].textContent == "X"
-    ) {
-      alert("GANASTE EN LA FILA DIAGONAL IZQUIERDA");
-    }
-    if (
-      cam[2].textContent == "X" &&
-      cam[4].textContent == "X" &&
-      cam[6].textContent == "X"
-    ) {
-      alert("GANASTE DIAGONAL DERECHA");
-    }
-    if (
-      cam[0].textContent == "O" &&
-      cam[1].textContent == "O" &&
-      cam[2].textContent == "O"
-    ) {
-      alert("O GANA ARRIBA EN LA FILA");
-    }
-
-    if (
-      cam[3].textContent == "O" &&
-      cam[4].textContent == "O" &&
-      cam[5].textContent == "O"
-    ) {
-      alert("O GANA EN LA FILA DEL MEDIO");
-    }
-
-    if (
-      cam[6].textContent == "O" &&
-      cam[7].textContent == "O" &&
-      cam[8].textContent == "O"
-    ) {
-      alert("O GANA EN LA FILA DE ABAJO");
-    }
-
-    if (
-      cam[0].textContent == "O" &&
-      cam[3].textContent == "O" &&
-      cam[6].textContent == "O"
-    ) {
-      alert("O GANA EN LA PRIMERA FILA VERTICAL");
-    }
-
-    if (
-      cam[1].textContent == "O" &&
-      cam[4].textContent == "O" &&
-      cam[7].textContent == "O"
-    ) {
-      alert("O GANA EN LA FILA VERTICAL DEL MEDIO");
-    }
-
-    if (
-      cam[2].textContent == "O" &&
-      cam[5].textContent == "O" &&
-      cam[8].textContent == "O"
-    ) {
-      alert("O GANA EN LA FILA VERTICAL DERECHA");
-    }
-    if (
-      cam[0].textContent == "O" &&
-      cam[4].textContent == "O" &&
-      cam[8].textContent == "O"
-    ) {
-      alert("O GANA EN LA FILA DIAGONAL IZQUIERDA");
-    }
-    if (
-      cam[2].textContent == "O" &&
-      cam[4].textContent == "O" &&
-      cam[6].textContent == "O"
-    ) {
-      alert("O GANA DIAGONAL DERECHA");
+    if (Winner(cam[i].textContent)) {
+      console.log(`${cam[i].textContent} ha ganado`);
     }
   });
+}
+function Winner(jugador) {
+  let win = false;
+  let combinacionesGanadoras = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8], // Filas
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8], // Columnas
+    [0, 4, 8],
+    [2, 4, 6], // Diagonales
+  ];
+
+  for (let cells of combinacionesGanadoras) {
+    if (
+      cam[cells[0]].textContent === jugador &&
+      cam[cells[1]].textContent === jugador &&
+      cam[cells[2]].textContent === jugador
+    ) {
+      win = true;
+      break;
+    }
+  }
+  return win;
 }
 
 let btnreinicio = document.getElementById("btn");
