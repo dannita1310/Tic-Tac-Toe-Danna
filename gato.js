abf892b5ddc4e42043510cf0bf922750d01655cb
Danna <gutierrezdanna119@gmail.com>
2023-06-30 14:02:33 -0600

    Proyecto Tic-Tac-Toe

--- gato.js
+++ gato.js
@@ -1,17 +1,9 @@
-var icon = document.getElementById("icon");
-icon.onclick = function () {
-  document.body.classList.toggle("dark-theme");
-  if (document.body.classList.contains("dark-theme")) {
-    icon.classList.add("active");
-  } else {
-    icon.classList.remove("active");
-  }
-};
-
 let cam = document.querySelectorAll(".box");
 let tab = document.getElementById("tablero");
 let Btn = document.getElementById("restartBtn");
-let playerM = "O";
+let gana = document.getElementById("gana");
+let computadora = "O";
+let juegoTerminado = false;
 
 const vari = [
   ["1", "2", "3"],
@@ -22,9 +14,8 @@ const vari = [
 function turnoO() {
   let camposVacios = Array.from(cam).filter(filtroVacios);
   let numero = Math.floor(Math.random() * camposVacios.length);
-  camposVacios[numero].innerHTML = playerM;
-  if (Winner(playerM)) {
-    console.log(`${playerM} ha ganado`);
+  if (camposVacios.length > 0) {
+    camposVacios[numero].innerHTML = computadora;
   }
 }
 
@@ -34,12 +25,27 @@ function filtroVacios(campo) {
 
 for (let i = 0; i < 9; i++) {
   cam[i].addEventListener("click", function () {
+    if(juegoTerminado){
+      return
+    }
     if (cam[i].textContent === "") {
       cam[i].innerHTML = "X";
-      turnoO();
-    }
-    if (Winner(cam[i].textContent)) {
-      console.log(`${cam[i].textContent} ha ganado`);
+      if (Winner(cam[i].textContent)) {
+        gana.innerHTML = `${cam[i].textContent} ha ganado`;
+        juegoTerminado = true;
+      } else {
+        turnoO();
+        if (Winner(computadora)) {
+          gana.innerHTML = `${computadora} ha ganado`;
+          juegoTerminado = true;
+        } else {
+          let camposVacios = Array.from(cam).filter(filtroVacios);
+          if (camposVacios.length === 0) {
+            gana.innerHTML = "Ha sido un empate!!!";
+            juegoTerminado = true;
+          }
+        }
+      }
     }
   });
 }
@@ -75,6 +81,3 @@ btnreinicio.addEventListener("click", reset);
 function reset() {
   window.location.reload();
 }
-
-let ganador = document.getElementById("ganador");
-ganador.innerHTML = "Ganador: Usuario";
